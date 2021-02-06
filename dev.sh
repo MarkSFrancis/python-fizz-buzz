6905cdc6d6cc64f2bfe3d2a577306423f9b61f84
Mark Francis <mrkfrancis8@gmail.com>
2021-02-06 20:16:31 +0000

    Delete dev.sh

--- dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$(which nodemon)" != ""];
-then
-  echo "Nodemon is required. Use 'npm i -g nodemon' to install nodemon"
-  exit 1
-fi
-
-nodemon -w **/*.py --exec "python3" app.py
